46d74e36a630cf10db786ff4cf50d7a3e117edf6
IvanZasukhin <120732767+IvanZasukhin@users.noreply.github.com>
2026-01-16 16:21:57 +0300

    Update init.py

--- streetview_hunter/init.py
+++ streetview_hunter/init.py
@@ -6,8 +6,10 @@ Google Street View Hunter
 """
 
 __version__ = "1.0.0"
-__author__ = "Ваше Имя"
-__email__ = "ваш.email@example.com"
+__author__ = "Иван Засухин"
+__email__ = "ivanzasukhin11@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/ваш-username/google-streetview-hunter"
 
 from .core import StreetViewHunter
 from .utils import load_config, save_config, validate_coordinates
